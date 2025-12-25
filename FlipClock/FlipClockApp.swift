a626f87228fa50b3d3f63201185f65ffb3678a96
orion_gz <asdfg60842@gmail.com>
2025-12-25 18:49:16 +0900

    Initial Commit

---
+++ FlipClock/FlipClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipClockApp.swift
+//  FlipClock
+//
+//  Created by 이정우 on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FlipClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
